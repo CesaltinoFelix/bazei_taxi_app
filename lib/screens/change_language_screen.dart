7b5e14bf275d77455521bb2823dbb50a3898a96b
CesaltinoFelix <cesaltinofelix2000@gmail.com>
2024-09-18 09:21:41 +0100

    new file first commit

---
+++ lib/screens/change_language_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChangeLanguageScreen extends StatefulWidget {
+  const ChangeLanguageScreen({super.key});
+
+  @override
+  State<ChangeLanguageScreen> createState() => _ChangeLanguageScreenState();
+}
+
+class _ChangeLanguageScreenState extends State<ChangeLanguageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
