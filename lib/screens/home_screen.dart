262d1774461af3474167a565312dece0baa3005e
CesaltinoFelix <cesaltinofelix2000@gmail.com>
2024-09-19 09:25:52 +0100

    adding home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // default constructor
+  MapController controller = MapController(
+    initPosition: GeoPoint(
+      latitude: 47.4358055, 
+      longitude: 8.4737324
+    ),
+  );
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          OSMFlutter(
+            controller: controller,
+            osmOption: OSMOption(
+              userTrackingOption: UserTrackingOption(
+                enableTracking: true,
+                unFollowUser: false,
+              ),
+              zoomOption: ZoomOption(
+                initZoom: 8,
+                minZoomLevel: 3,
+                maxZoomLevel: 19,
+                stepZoom: 1.0,
+              ),
+              userLocationMarker: UserLocationMaker(
+                personMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.location_history_rounded,
+                    color: Colors.red,
+                    size: 48,
+                  ),
+                ),
+                directionArrowMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.double_arrow,
+                    size: 48,
+                  ),
+                ),
+              ),
+              roadConfiguration: RoadOption(
+                roadColor: Colors.yellowAccent,
+              ),
+              // Atualize conforme a documentação mais recente
+              // Ajuste ou remova markerOption se não for mais necessário
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
